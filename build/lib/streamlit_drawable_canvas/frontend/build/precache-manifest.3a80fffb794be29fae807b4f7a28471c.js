58b8b1f3867e4e87ad054fbeeb62fc8928dc52d5
Pavlo Grinevich <grinevichpavlo@gmail.com>
2022-10-31 12:57:18 +0200

    f

---
+++ build/lib/streamlit_drawable_canvas/frontend/build/precache-manifest.3a80fffb794be29fae807b4f7a28471c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7f4d325a8d31236e647a0e0a417385",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58e1c94077ede649209c",
+    "url": "./static/css/main.41cd645d.chunk.css"
+  },
+  {
+    "revision": "86de789b22ff8657e8eb",
+    "url": "./static/js/2.9978a2c0.chunk.js"
+  },
+  {
+    "revision": "f1fb474ae9ccd77a5fa1bda403be7c43",
+    "url": "./static/js/2.9978a2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e1c94077ede649209c",
+    "url": "./static/js/main.b77e200e.chunk.js"
+  },
+  {
+    "revision": "5077e72dc1f115d18129",
+    "url": "./static/js/runtime-main.b59b7c8b.js"
+  }
+]);
